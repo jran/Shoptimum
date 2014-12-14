d96e031dfe78225ec28e777d5e2211d3833f9dc5
Sally Kong <kongsally94@outlook.com>
2014-12-13 19:55:52 -0500

    Submit Style Works

---
+++ js/submit.js
@@ -0,0 +1,22 @@
+
+$(function() {
+ 
+  Parse.$ = jQuery;
+  Parse.initialize("mQBIpuA3C7kVDNvG2fBxwUhahFi7S5dEIVFN0bKT",
+                   "q7ltha9y0qHSo9TDWOdUpO72U0zWAvZuSPpw3imA");
+});
+
+function saveStyle() {
+	console.log("hmm");
+	var lookStyle = Parse.Object.extend("Style");
+	var newStyle = new lookStyle();
+
+	var styleName = $("#imageName").val();
+	var styleURL = $("#imageURL").val();
+
+	newStyle.set("Name", styleName);
+	newStyle.set("Image", styleURL);
+	newStyle.save();
+
+	 $('input').val('');
+}
